238766455f444271291113318153d07e82cb2e65
Bill <bpenda94@gmail.com>
2016-02-01 20:53:24 -0600

    gyro and mag

--- runomatic/ViewController.swift
+++ runomatic/ViewController.swift
@@ -8,26 +8,43 @@
 
 import UIKit
 import CoreMotion
+import Foundation
 var motionManager: CMMotionManager!
 
 
 class ViewController: UIViewController {
+    let pi = M_PI
     @IBOutlet var time:UILabel!
-    @IBOutlet var xaccl:UILabel!
-    @IBOutlet var yaccl:UILabel!
-    @IBOutlet var zaccl:UILabel!
+    @IBOutlet var xal:UILabel!
+    @IBOutlet var yal:UILabel!
+    @IBOutlet var zal:UILabel!
+    @IBOutlet var xrl:UILabel!
+    @IBOutlet var yrl:UILabel!
+    @IBOutlet var zrl:UILabel!
+    @IBOutlet var xml:UILabel!
+    @IBOutlet var yml:UILabel!
+    @IBOutlet var zml:UILabel!
+    
+    var startTime:NSDate = NSDate()
+    var elapsedTime:NSTimeInterval = 0.0
     var xa:Double = 0,ya:Double = 0,za:Double = 0
+    var xr:Double = 0,yr:Double = 0,zr:Double = 0
+    var xm:Double = 0,ym:Double = 0,zm:Double = 0
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         motionManager = CMMotionManager()
         motionManager.startAccelerometerUpdates()
-        var helloWorldTimer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("getAccel"), userInfo: nil, repeats: true)
+        motionManager.startGyroUpdates()
+        motionManager.startMagnetometerUpdates()
 
+        _ = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("getReadings"), userInfo: nil, repeats: true)
+        startTime = NSDate()
         time.text = "hi ben"
-        self.xaccl.text = "x"
-        self.yaccl.text = "y"
-        self.zaccl.text = "x"
+        self.xal.text = "x"
+        self.yal.text = "y"
+        self.zal.text = "z"
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,24 +52,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func getAccel(){
+    func getReadings(){
         
         let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
         dispatch_async(dispatch_get_global_queue(priority, 0)) {
             if let accelerometerData = motionManager.accelerometerData {
-                self.xa = accelerometerData.acceleration.x
-                self.ya = accelerometerData.acceleration.y
-                self.za = accelerometerData.acceleration.z
+                self.xa = accelerometerData.acceleration.x * 9.8
+                self.ya = accelerometerData.acceleration.y * 9.8
+                self.za = accelerometerData.acceleration.z * 9.8
+                self.elapsedTime = NSDate().timeIntervalSinceDate(self.startTime)
+
+            }
+            if let gyroData = motionManager.gyroData {
+                self.xr = gyroData.rotationRate.x*180/self.pi
+                self.yr = gyroData.rotationRate.y*180/self.pi
+                self.zr = gyroData.rotationRate.z*180/self.pi
+                
+            }
+            if let magData = motionManager.magnetometerData {
+                self.xm = magData.magneticField.x
+                self.ym = magData.magneticField.y
+                self.zm = magData.magneticField.z
                 
             }
             dispatch_async(dispatch_get_main_queue()) {
-                self.xaccl.text = String(self.xa)
-                self.yaccl.text = String(self.ya)
-                self.zaccl.text = String(self.za)
+                self.xal.text = String(format: "%.2f", self.xa)
+                self.yal.text = String(format: "%.2f", self.ya)
+                self.zal.text = String(format: "%.2f", self.za)
+                self.xrl.text = String(format: "%.2f", self.xr)
+                self.yrl.text = String(format: "%.2f", self.yr)
+                self.zrl.text = String(format: "%.2f", self.zr)
+                self.xml.text = String(format: "%.2f", self.xm)
+                self.yml.text = String(format: "%.2f", self.ym)
+                self.zml.text = String(format: "%.2f", self.zm)
+
+                self.time.text = String(self.elapsedTime)
+
             }
         }
 
-            }
+    }
 
 }
 
